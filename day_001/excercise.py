3368b23f47ff4ae45281ec944f6e53d779763859
Manav Chaudhary <vrmanav02@gmail.com>
2023-02-25 00:02:55 +0530

    Day 1 ✓

---
+++ day_001/excercise.py
@@ -0,0 +1,23 @@
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# # Use single quotes to print out the string
+# print("print('what to print')")
+
+# print("Day 1 - String Manipulation")
+
+# print("String Concatenation is done with the \"+\" sign.")
+# # Alternate way to print double quotes inside print()
+# print('String Concatenation is done with the "+" sign.')
+
+# print("e.g. print(\"Hello \" + \"world\")")
+# print("New lines can be created with  a backslash and n.")
+
+# print(len(input("What is your name - ")))
+
+# a = input("Enter the value of A - ")
+# b = input("Enter the value of B - ")
+# c = a
+# a = b
+# b = c
+# print("Value of A - " + a)
+# print("Value of B - " + b)
---
+++ day_001/main.py
@@ -0,0 +1,7 @@
+
+#! DAY 1 - Band Name Generator
+print("Welcome to Band Name Generator.")
+city_name = input("What's the name of the city you grew up in - \n")
+pet_name = input("What's the name of your pet - \n")
+band_name = city_name + pet_name
+print("Your band's name is - " + band_name)
